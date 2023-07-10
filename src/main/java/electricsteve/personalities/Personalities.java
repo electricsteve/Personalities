81009c6a65b940d0ad0ba628e4bbe925216d2ff8
ElectricSteve <nathanndees@gmail.com>
2023-07-10 15:21:44 +0200

    Initial Commit

---
+++ src/main/java/electricsteve/personalities/Personalities.java
@@ -0,0 +1,19 @@
+package electricsteve.personalities;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Personalities extends JavaPlugin {
+    FileConfiguration config = getConfig();
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
